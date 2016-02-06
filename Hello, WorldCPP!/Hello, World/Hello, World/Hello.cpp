900b47f20fa41113a8df5f559690a987b4ae45b0
Tolyan91 <klaviat91@mail.ru>
2016-02-06 11:34:46 +0600

    добавлена функция calc(калькулятор)

--- Hello, WorldCPP!/Hello, World/Hello, World/Hello.cpp
+++ Hello, WorldCPP!/Hello, World/Hello, World/Hello.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
 using namespace std; 
 
+void calc()
+{
+	double a;
+	double b;
+	double r;
+	char s;
+	cout << "Vvesti pervoe chislo: ";
+	cin >> a;
+	cout << "vvesti vtoroe chislo: ";
+	cin >> b;
+	cout << "Vvedite operaciy kotoruy nado proizvesti:\n";
+	cout << "+, -, /, *\n";
+	cin >> s;
+	switch (s)
+	{
+		case '+':	r = a + b;	break;
+		case '-':	r = a - b;	break;
+		case '/':	r = a / b;	break;
+		case '*':	r = a * b;	break;
+
+		default: cout << "neverniy simvol\n";
+			break;
+	}
+	cout << "a " << s << " b = r" << endl;
+	cout << a <<" "<< s <<" "<< b << " = " << r << endl;
+}
+
 int main()
 {
-	cout << "Hello, World!" << endl;
-	cout << "Testing git client!" << endl;
-	cout << "new commit!" << endl;
+	calc();
+
+
 	system("pause");
 	return 0;
 }
